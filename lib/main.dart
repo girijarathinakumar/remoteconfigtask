a1159a19a6677be9d02745453d9ccf6e1a24ddb0
girija <girija202001@gmail.com>
2023-10-09 15:00:39 +0530

    remoteconfigchanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,7 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'firebase_options.dart';
-// import 'home_screen.dart';
-//
-// Future<void> main()async {
-//     WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//   home: HomePage(),
-//     );
-//   }
-// }
-// class HomePage extends StatefulWidget {
-//
-//
-//   const HomePage({super.key});
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:remoteconfig/remote_config.dart';
+import 'app_theme.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -49,75 +11,52 @@ Future<void> main() async {
   );
   await FirebaseRemoteConfigService().initialize();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
-   MyApp({super.key});
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-   final remoteConfig = FirebaseRemoteConfigService();
+    final remoteConfig = FirebaseRemoteConfigService();
     // String theme='theme_mode';
     return MaterialApp(
-          theme: ThemeData.light(), // Default light theme
-          darkTheme: ThemeData.dark(), // Default dark theme
-          themeMode:remoteConfig == 'dark' ?ThemeMode.dark :ThemeMode.light,
-          home: const HomeScreen(),
-        );}}
+      theme: ThemeData.light(), // Default light theme
+      darkTheme: ThemeData.dark(), // Default dark theme
+      themeMode:
+          remoteConfig.themeCode == 'dark' ? ThemeMode.dark : ThemeMode.light,
+      home: const HomeScreen(),
+    );
+  }
+}
+
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({super.key});
-
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-// String theme='';
-//     void initState(){
-//       fetchStringConfig();
-//     super.initState();
-//   }
   @override
   Widget build(BuildContext context) {
     final remoteConfig = FirebaseRemoteConfigService();
 
     return Scaffold(
       body: Center(
-        child: Text(
-          remoteConfig.welcomeMessage,
-          textAlign: TextAlign.center,
+        child: TextButton(
+          child: Text(
+            remoteConfig.themeCode,
+            textAlign: TextAlign.center,
+
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (ctx) => ScreenPage()));
+          },
         ),
       ),
     );
-
-
   }
-// Future fetchStringConfig()async{
-//
-//   final remoteConfig = FirebaseRemoteConfig.instance;
-//   await remoteConfig.setConfigSettings(RemoteConfigSettings(
-//       fetchTimeout: const Duration(minutes: 1),
-//       minimumFetchInterval: const Duration(minutes: 1 )));
-//   await remoteConfig.fetchAndActivate();
-//   theme = remoteConfig.getString("theme_mode");
-//   setState(() {
-//
-//   });
-// }
-
 }
-// Future<void> fetchRemoteConfig() async {
-//   try {
-//     FirebaseRemoteConfig remoteConfig = await FirebaseRemoteConfig.instance;
-//     await remoteConfig.fetch();
-//     await remoteConfig.activate();
-//     final String welcomeMessage = remoteConfig.getString('theme_mode');
-//     log('Welcome Message: $welcomeMessage');
-//   } catch (e) {
-//     print('Error fetching remote config: $e');
-//   }
-// }
--- lib/remote_config.dart
+++ lib/remote_config.dart
@@ -1,24 +1,32 @@
-
-
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/cupertino.dart';
 
 class FirebaseRemoteConfigService {
-  FirebaseRemoteConfigService._() : _remoteConfig = FirebaseRemoteConfig.instance;
+  FirebaseRemoteConfigService._()
+      : _remoteConfig = FirebaseRemoteConfig.instance;
 
-  String get welcomeMessage => _remoteConfig.getString(FirebaseRemoteConfigKeys.welcomeMessage);
+  String get themeCode =>
+      _remoteConfig.getString(FirebaseRemoteConfigKeys.themeCode);
+  // String get fontSize => _remoteConfig.getString(FirebaseRemoteConfigKeys.fontSize);
+  String get title => _remoteConfig.getString(FirebaseRemoteConfigKeys.title);
+   String get image => _remoteConfig.getString(FirebaseRemoteConfigKeys.image);
+   String get remoteTextStyle => _remoteConfig.getString(FirebaseRemoteConfigKeys.remoteTextStyle.toString());
+  // String get defaultTextStyle => _remoteConfig.getString(FirebaseRemoteConfigKeys.defaultTextStyle.toString());
 
   static FirebaseRemoteConfigService? _instance;
-  factory FirebaseRemoteConfigService() => _instance ??= FirebaseRemoteConfigService._();
+
+  factory FirebaseRemoteConfigService() =>
+      _instance ??= FirebaseRemoteConfigService._();
 
   final FirebaseRemoteConfig _remoteConfig;
 
   Future<void> _setConfigSettings() async => _remoteConfig.setConfigSettings(
-    RemoteConfigSettings(
-      fetchTimeout: const Duration(minutes: 1),
-      minimumFetchInterval: const Duration(hours: 12),
-    ),
-  );
+        RemoteConfigSettings(
+          fetchTimeout: const Duration(minutes: 1),
+          minimumFetchInterval: const Duration(seconds: 10),
+        ),
+      );
+
   Future<void> fetchAndActivate() async {
     bool updated = await _remoteConfig.fetchAndActivate();
 
@@ -28,13 +36,29 @@ class FirebaseRemoteConfigService {
       debugPrint('The config is not updated..');
     }
   }
+
   Future<void> initialize() async {
     await _setConfigSettings();
     await fetchAndActivate();
+
+
   }
 }
 
 class FirebaseRemoteConfigKeys {
-  static const String welcomeMessage = 'theme_mode';
+  double fontSize = 20;
+
+  static FirebaseRemoteConfig  remoteConfig = FirebaseRemoteConfig.instance;
+  static const String themeCode = 'theme_mode';static const String title = 'title';
+  static const String image = 'Image';
+  static const String color = 'title_color';
+
+  static  TextStyle remoteTextStyle = TextStyle(
+    fontSize: remoteConfig.getDouble('font_size'),
+    color: Color(remoteConfig.getInt('title_color')),
+    // Other text style properties from remote config
+  );
+
+
 }
 
